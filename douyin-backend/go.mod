ea23badb0d3f8b5afbd7adac1fdd7b8efbe126f6
yc <1747683288@qq.com>
2022-06-08 16:45:40 +0800

    视频上传temp1

--- douyin-backend/go.mod
+++ douyin-backend/go.mod
@@ -6,10 +6,12 @@ require (
 	github.com/disintegration/imaging v1.6.2
 	github.com/gin-gonic/gin v1.8.1
 	github.com/jinzhu/gorm v1.9.16
+	github.com/u2takey/ffmpeg-go v0.4.1
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
+	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -17,12 +19,14 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
--- douyin-backend/go/controller/VideoController.go
+++ douyin-backend/go/controller/VideoController.go
@@ -1,8 +1,15 @@
 package controller
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/disintegration/imaging"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"io"
+	"os"
+	"strconv"
+	"time"
+
 	//gin框架的依赖
 	"github.com/gin-gonic/gin"
 	//http连接包
@@ -22,9 +29,30 @@ func PostVideo(c *gin.Context) {
 		return
 	}
 
+	//接token
+	//var user entity.User
+	//user.Name = c.Query("username")
+	//user.Password = c.Query("password")
+	//
+	//user, _ = service.GetUserByName(user.Name)
+
+	//接title,这里接了后面没用到，是存到数据库的，数据库还没设计，所以我就先注释了
+	/*
+		title, ok := c.GetPostForm("title")
+		if !ok {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+	*/
+	//一个不知道我们要不要的异常处理，大概是测试路径的？
+	_, err = os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
 	//上传file文件到指定的文件路径
+	//path，video_name
 	path := "111"
-	video_name := "111" + ".mp4"
+	video_name := GenerateFilename("ycc", 111) + ".mp4"
 	fn := path + video_name
 	if err := c.SaveUploadedFile(file, fn); err != nil {
 		fmt.Println("存储视频失败，filePath,", fn)
@@ -33,12 +61,13 @@ func PostVideo(c *gin.Context) {
 	}
 
 	//读取视频第一帧作为封面
-	//reader := utils.ReadFrameAsJpeg(fn)
+	reader := ReadFrameAsJpeg(fn)
 	img, err := imaging.Decode(reader)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	//image_path
 	image_path := "222"
 
 	// replace .mp4 to .jpg
@@ -51,6 +80,7 @@ func PostVideo(c *gin.Context) {
 	//把它们给存到数据库里面
 	//play_url := video_name
 	//cover_url := image_name
+	//title
 
 	c.JSON(http.StatusOK, PublishInfoResponse{
 		status_code: 0,
@@ -58,12 +88,33 @@ func PostVideo(c *gin.Context) {
 	})
 
 	//todo
-	//传过来的token没接，要鉴权，已登陆？
-	//视频数据库没建
-	//视频封面处理还没做
-	//视频保存路径，封面保存路径，video_name
-	//白天再继续完成这些吧
+	//传过来的token不知道怎么解析
+	//视频数据库没建,所以数据保存还没做
+	//视频保存路径，封面保存路径，video_name,
+	//错误处理有点不太懂
+	//以及别人的不太清楚怎么改一下，直接用是不是不太好，目前直接照搬过来了，不过我去看了一下别人用到的库
+	//而且还没测试这一块
+}
 
+// GenerateFilename depend on timestamp, username and user id to generate a filename for uploaded videos
+func GenerateFilename(username string, userId int64) string {
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	return timestamp + "_" + username + "_" + strconv.FormatInt(userId, 10)
+}
+
+// ReadFrameAsJpeg use ffmpeg read first frame of video as a jpeg
+func ReadFrameAsJpeg(inFileName string) io.Reader {
+	frameNum := 1
+	buf := bytes.NewBuffer(nil)
+	err := ffmpeg.Input(inFileName).
+		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
+		Output("pipe:", ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "mjpeg"}).
+		WithOutput(buf, os.Stdout).
+		Run()
+	if err != nil {
+		panic(err)
+	}
+	return buf
 }
 
 /**
